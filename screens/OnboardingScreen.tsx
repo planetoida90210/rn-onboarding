470d954be599fe41f20d2fd8858869d875702e7b
Michał Bryk <galeriakoloru90210@gmail.com>
2023-03-14 21:41:42 +0100

    initialize onboarding library

--- screens/OnboardingScreen.tsx
+++ screens/OnboardingScreen.tsx
@@ -1,11 +1,25 @@
-import { Text, SafeAreaView } from "react-native";
+import { Text, SafeAreaView, Image } from "react-native";
 import React from "react";
+import Onboarding from "react-native-onboarding-swiper";
 
 const OnboardingScreen = () => {
   return (
-    <SafeAreaView>
-      <Text>OnboardingScreen</Text>
-    </SafeAreaView>
+    <Onboarding
+      pages={[
+        {
+          backgroundColor: "#fff",
+          image: (
+            <Image
+              source={{
+                uri: "https://www.pngfind.com/pngs/m/57-572728_image-naruto-png-naruto-shippuden-naruto-uzumaki-naruto.png",
+              }}
+            />
+          ),
+          title: "Onboarding",
+          subtitle: "Done with React Native Onboarding Swiper",
+        },
+      ]}
+    />
   );
 };
 
