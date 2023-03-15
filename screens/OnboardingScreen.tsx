466cba0656e5e63b7087c3920e58b2a77bfb7b1a
Michał Bryk <galeriakoloru90210@gmail.com>
2023-03-15 21:50:52 +0100

    setting replace on skip button

--- screens/OnboardingScreen.tsx
+++ screens/OnboardingScreen.tsx
@@ -1,10 +1,15 @@
 import { Text, SafeAreaView, Image } from "react-native";
 import React from "react";
 import Onboarding from "react-native-onboarding-swiper";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../App";
 
 const OnboardingScreen = () => {
+  const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
   return (
     <Onboarding
+      onSkip={() => navigation.replace("Home")}
       pages={[
         {
           backgroundColor: "#fff",
@@ -13,10 +18,37 @@ const OnboardingScreen = () => {
               source={{
                 uri: "https://www.pngfind.com/pngs/m/57-572728_image-naruto-png-naruto-shippuden-naruto-uzumaki-naruto.png",
               }}
+              className="w-72 h-72 object-contain"
             />
           ),
-          title: "Onboarding",
-          subtitle: "Done with React Native Onboarding Swiper",
+          title: "Happy Discovering",
+          subtitle: "Discover the app with latest features",
+        },
+        {
+          backgroundColor: "#fff",
+          image: (
+            <Image
+              source={{
+                uri: "https://www.pngfind.com/pngs/m/57-572728_image-naruto-png-naruto-shippuden-naruto-uzumaki-naruto.png",
+              }}
+              className="w-72 h-72 object-contain"
+            />
+          ),
+          title: "All You need in one place",
+          subtitle: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        },
+        {
+          backgroundColor: "#fff",
+          image: (
+            <Image
+              source={{
+                uri: "https://www.pngfind.com/pngs/m/57-572728_image-naruto-png-naruto-shippuden-naruto-uzumaki-naruto.png",
+              }}
+              className="w-72 h-72 object-contain"
+            />
+          ),
+          title: "Check the new features",
+          subtitle: "Lorem ipsum dolo sit amet",
         },
       ]}
     />
