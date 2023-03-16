d19993e23b8d50c0d0ba29c936460e5b015e5b0c
Michał Bryk <galeriakoloru90210@gmail.com>
2023-03-16 21:54:53 +0100

    set dotcomponent with typescript

--- screens/OnboardingScreen.tsx
+++ screens/OnboardingScreen.tsx
@@ -1,15 +1,34 @@
-import { Text, SafeAreaView, Image } from "react-native";
-import React from "react";
-import Onboarding from "react-native-onboarding-swiper";
+import { Text, SafeAreaView, Image, View } from "react-native";
+import React, { ButtonHTMLAttributes } from "react";
+import Onboarding, { SkipButtonProps } from "react-native-onboarding-swiper";
 import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../App";
 
+interface DotProps {
+  selected: boolean;
+}
+
 const OnboardingScreen = () => {
   const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+
+  const DotComponent = ({ selected }: DotProps) => {
+    return (
+      <View
+        className={`w-4 h-4 mx-1 flex items-center justify-center rounded-full ${
+          selected ? "border border-orange-400" : ""
+        }`}
+      >
+        <View className={`w-2 h-2 ${selected ? "bg-orange-400" : "bg-orange-200"} rounded-full`}></View>
+      </View>
+    );
+  };
+
   return (
     <Onboarding
       onSkip={() => navigation.replace("Home")}
+      onDone={() => navigation.replace("Home")}
+      DotComponent={DotComponent}
       pages={[
         {
           backgroundColor: "#fff",
